bd493914ba2c5f11e3c57b068cbd63ab8de139ae
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-24 10:34:39 +0300

    Added an onclick function to the button

--- src/components/App.js
+++ src/components/App.js
@@ -15,15 +15,14 @@ function App() {
 
   // replace 'false' with a state variable that can be toggled between true and false
   // this will be used for the Dark Mode Toggle feature
-  const { container } = render(<App />)
-  container.querySelector(".light")
+
   const appClass = false ? "App dark" : "App light"
 
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button >Dark Mode</button>
+        <button onClick={handleClick}>{isDark? "Dark ":"Light "} Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
