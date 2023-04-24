62c682142d561d604a699f7736ce4c39074adbe1
Sang Kibet Elkana <elkanahkibet6@gmail.com>
2023-04-24 14:40:03 +0300

    Added useState for the shoppinglist

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [sectedCat, setsectedCat] = useState(items);
+      function handleChange(event){
+        const cart = event.target.value;
+        const cartItems = items.filter((item)=> item.category ===cart)
+        setsectedCat(cartItems)
+      }
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +20,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {sectedCat.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
